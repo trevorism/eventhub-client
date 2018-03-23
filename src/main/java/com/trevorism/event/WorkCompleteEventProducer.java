f8f25a932ab7211d98f49fa1a1201cea00cadec6
Trevor Brooks <trevorism@gmail.com>
2018-03-23 11:22:53 -0400

    EVenthub client public work complete

--- src/main/java/com/trevorism/event/WorkCompleteEventProducer.java
+++ src/main/java/com/trevorism/event/WorkCompleteEventProducer.java
@@ -7,7 +7,7 @@ import com.trevorism.event.model.WorkComplete;
  */
 public class WorkCompleteEventProducer extends PingingEventProducer<WorkComplete> {
 
-    void sendEvent(WorkComplete workComplete){
+    public void sendEvent(WorkComplete workComplete){
         super.sendEvent(WorkComplete.TOPIC, workComplete, workComplete.getCorrelationId());
     }
 
